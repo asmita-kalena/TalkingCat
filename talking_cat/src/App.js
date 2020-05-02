9c0cf1bc0cc634a0e8f83c60f446066597c80a55
asmita-kalena <asmita.kalena.23@gmail.com>
2020-05-03 03:56:28 +0530

    Final code

--- talking_cat/src/App.js
+++ talking_cat/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<blockquote>
+					<div id="speech"></div>
+					<div id="bottom1"></div>
+				</blockquote>
+				<div id="cat">😼</div>
+				<button id="go" onClick={go}>
+					go()
+				</button>
+			</header>
+		</div>
+	);
+}
+
+function go() {
+	console.log("calling...");
+	var messages = [
+		"Meow!",
+		"I'm a talking cat!",
+		"Callbacks are fun!",
+		"Okay, bye!",
+	];
+
+	// old one
+	// for (var i = 0; i < messages.length; i++) {
+	// 	setTimeout(() => {
+	// 		cat.say(messages[i]);
+	// 	}, i * 1500);
+	// }
+
+	// updated one
+	messages.forEach(function (message, i) {
+		setTimeout(() => {
+			cat.say(message);
+		}, i * 1500);
+	});
+
+	var cat = {
+		speech: document.getElementById("speech"),
+		cat: document.getElementById("cat"),
+		say: function (quip) {
+			this.speech.textContent = "" + quip;
+			this.cat.textContent = "😸";
+			setTimeout(() => {
+				this.speech.textContent = "";
+				this.cat.textContent = "😼";
+			}, 1200);
+		},
+	};
 }
 
 export default App;
